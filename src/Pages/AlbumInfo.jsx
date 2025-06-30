beb2bb23e797e8c425c18b10aed63e2d76a2c142
Terence <tjsaverin98@live.com>
2025-06-30 07:46:36 -0500

    React css query

--- src/Pages/AlbumInfo.jsx
+++ src/Pages/AlbumInfo.jsx
@@ -1,32 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Nav from '../compononents/Nav';
-import styles from '../AlbumInfo.module.css'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Nav from "../compononents/Nav";
+import styles from "../AlbumInfo.module.css";
 
 const AlbumInfo = () => {
-  const { name } = useParams()
-  console.log(name)
-
+  const { name } = useParams();
+  console.log(name);
 
   async function fetchAlbumInfo() {
-    const { data } = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=01a9bc49bbc9abed2dd1966234ac875e&artist=Cher&album=Believe&format=json`);
-    console.log(data)
+    const { data } = await axios.get(
+      `https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=01a9bc49bbc9abed2dd1966234ac875e&artist=Cher&album=Believe&format=json`
+    );
+    console.log(data);
   }
 
-  useEffect(() => { 
-    fetchAlbumInfo()
+  useEffect(() => {
+    fetchAlbumInfo();
   }, []);
 
   return (
-    <div>
+    <div classname={styles}>
       <Nav />
-      <div className={styles}>
-     {name}
+      <div className="container">
+        <div className="row">
+        <div>{name}</div>
+        <button>Button</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AlbumInfo;
