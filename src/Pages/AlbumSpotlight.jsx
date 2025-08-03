b022acf4035d3525b99c8a2a0507e818528fd8db
Terence <tjsaverin98@live.com>
2025-08-03 06:48:57 -0500

    project submission

--- src/Pages/AlbumSpotlight.jsx
+++ src/Pages/AlbumSpotlight.jsx
@@ -150,11 +150,11 @@ const AlbumSpotlight = () => {
                 </div>
               </>
             )}
-            <div className="albums__container">
+            {/* <div className="albums__container">
               <div className="row">
               <span className="yellow" style={{ marginTop: "14px", fontFamily: "cursive" }}>this space will hold the albums related to the spotlighted albums</span>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
